ccfe5a3f4f376ab93e6ddb2bd8260f8b0911b753
Jasterix C <loune.calixte@gmail.com>
2019-04-19 00:35:14 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -54,7 +54,7 @@ def get_average_age_for_season(data, season)
   end
   # return ages
    avg = ages.sum / ages.length
-  
+  avg.round
 end
 
 #////////////////////////////////////////////////NOTES
