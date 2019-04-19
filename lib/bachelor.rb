d4f8fdccaad59f4ad997c147d8396b279487cc5c
Jasterix C <loune.calixte@gmail.com>
2019-04-19 00:16:29 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -47,7 +47,7 @@ end
 
 def get_average_age_for_season(data, season)
   # code here
-  ages = data[season].each do |person| person["age"] 
+  ages = data[season].collect do |person| person["age"] 
   
       end
       # binding.pry
